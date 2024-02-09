375ac9f42f46ec98cc906f3382bc81d7dd87afd4
carlacodes <carla.grifiths@gmail.com>
2024-02-09 16:23:24 +0000

    adding xy position to the dataloader

--- helpers/datahandling.py
+++ helpers/datahandling.py
@@ -30,6 +30,7 @@ class DataHandler():
         fs = ((sample[0][0] / ts[0][0]) * 10000)[0]
 
         dlc_angle = hcomb_data['dlc_angle']
+        dlc_xy = hcomb_data['dlc_XYsmooth']
 
         df_all = pd.DataFrame()
 
@@ -38,9 +39,10 @@ class DataHandler():
             spike_times = unit['spikeSamples'][0].astype(float) / fs
 
             head_angle_times = np.array([])
-            dlc_angle_list = np.array([])
+            dlc_angle_array = np.array([])
             head_angle_times_ms = np.array([])
             trial_number_array = np.array([])
+            xy_pos_array = np.array([])
 
             for i2 in range(len(dlc_angle)):
                 trial_dlc, trial_ts, trial_sample = dlc_angle[i2], ts[i2], sample[i2]
@@ -51,7 +53,9 @@ class DataHandler():
 
                 head_angle_times = np.append(head_angle_times, time_in_seconds)
                 head_angle_times_ms = np.append(head_angle_times_ms, trial_ts)
-                dlc_angle_list = np.append(dlc_angle_list, trial_dlc)
+                dlc_angle_array = np.append(dlc_angle_array, trial_dlc)
+                dlc_xy_array = np.append(xy_pos_array, dlc_xy[i2])
+
 
                 if np.max(time_in_seconds) > np.max(spike_times):
                     print('Trial time is greater than spike time, aborting...')
@@ -61,8 +65,9 @@ class DataHandler():
 
             flattened_spike_times_seconds = np.concatenate(spike_times).ravel()
             flattened_spike_times = np.concatenate(unit['spikeSamples']).ravel()
-            dlc_new = np.interp(flattened_spike_times_seconds*1000, head_angle_times_ms, dlc_angle_list)
+            dlc_new = np.interp(flattened_spike_times_seconds*1000, head_angle_times_ms, dlc_angle_array)
             trial_new = np.interp(flattened_spike_times_seconds*1000, head_angle_times_ms, trial_number_array)
+            xy_pos_new = np.interp(flattened_spike_times_seconds*1000, head_angle_times_ms, dlc_xy_array)
 
             # Create DataFrame for the current unit
             unit_id = np.full(len(flattened_spike_times), unit['name'][0].astype(str))
@@ -76,10 +81,12 @@ class DataHandler():
                 'spike_times_seconds': flattened_spike_times_seconds,
                 'spike_times_samples': flattened_spike_times,
                 'dlc_angle': dlc_new,
+                'dlc_xy': xy_pos_new,
                 'unit_id': unit_id,
                 'phy_cluster': phy_cluster,
                 'neuron_type': neuron_type,
                 'trial_number': trial_new
+
             })
 
             # Append to the larger dataframe
