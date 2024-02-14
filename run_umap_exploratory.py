ef3c2f1248a412030a7c82d35b4043de3874ce8e
carlacodes <carla.grifiths@gmail.com>
2024-02-14 16:33:31 +0000

    changing Jules' code for SVR rather than SVC

--- run_umap_exploratory.py
+++ run_umap_exploratory.py
@@ -1,8 +1,7 @@
-#spks i sa numpy array of size trial* timebins*neuron
-#bhv is a pandas dataframe where each row represents a trial
-
 from pathlib import Path
 from datetime import datetime
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.dummy import DummyRegressor
 import json
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -22,63 +21,65 @@ from sklearn.model_selection import RepeatedStratifiedKFold, StratifiedKFold, pe
     RandomizedSearchCV, cross_val_score
 from sklearn.svm import SVC
 from sklearn.metrics import balanced_accuracy_score, f1_score
-
 from sklearn.dummy import DummyClassifier
-
+from sklearn.metrics import mean_squared_error, r2_score
 from umap import UMAP
 # import vowel_in_noise.electrophysiology.population_analysis as vowel_pop
 # from vowel_in_noise import plot_utils
-''' Modified from Jules Lebert's code'''
+from sklearn.model_selection import train_test_split
+''' Modified from Jules Lebert's code
+spks is a numpy arrray of size trial* timebins*neuron, and bhv is  a pandas dataframe where each row represents a trial, the trial is the index '''
 
 def process_window(
         w,
-        ref_spks,
-        nref_spks,
+        spks,
         window_size,
-        y_ref,
-        y_test,
+        y,
         reducer_pipeline,
-        classifier,
-        classifier_kwargs,
+        regressor,
+        regressor_kwargs,
 ):
-    clf = classifier(**classifier_kwargs)
+    reg = regressor(**regressor_kwargs)
 
-    window_ref = ref_spks[:, w:w + window_size, :].reshape(ref_spks.shape[0], -1)
-    window_nref = nref_spks[:, w:w + window_size, :].reshape(nref_spks.shape[0], -1)
+    window = spks[:, w:w + window_size, :].reshape(spks.shape[0], -1)
 
-    # Fit the reducer on the reference space
-    reducer_pipeline.fit(window_ref, y=y_ref)
+    # Split the data into training and testing sets
+    window_train, window_test, y_train, y_test = train_test_split(window, y, test_size=0.2, random_state=42)
 
-    # Transform the reference and non-reference space
-    window_ref_reduced = reducer_pipeline.transform(window_ref)
-    window_nref_reduced = reducer_pipeline.transform(window_nref)
+    # Fit the reducer on the training data
+    reducer_pipeline.fit(window_train, y=y_train)
 
-    # Fit the classifier on the reference space
-    clf.fit(window_ref_reduced, y_ref)
+    # Transform the training and testing data
+    window_train_reduced = reducer_pipeline.transform(window_train)
+    window_test_reduced = reducer_pipeline.transform(window_test)
 
-    # Predict on the non-reference space
-    y_pred = clf.predict(window_nref_reduced)
+    # Fit the regressor on the training data
+    reg.fit(window_train_reduced, y_train)
 
-    # Compute the accuracy
-    ba_score = balanced_accuracy_score(y_test, y_pred)
-    f1_score_val = f1_score(y_test, y_pred, average='weighted')
+    # Predict on the testing data
+    y_pred = reg.predict(window_test_reduced)
+
+    # Compute the mean squared error and R2 score
+    mse_score = mean_squared_error(y_test, y_pred)
+    r2_score_val = r2_score(y_test, y_pred)
 
     results = {
-        'ba_score': ba_score,
-        'f1_score': f1_score_val,
+        'mse_score': mse_score,
+        'r2_score': r2_score_val,
         'w': w,
     }
 
     return results
 
 
+
+
 def train_ref_classify_rest(
         spks,
         bhv,
-        space_ref,
-        classify,
-        classifier,
-        classifier_kwargs,
+        regress,
+        regressor,
+        regressor_kwargs,
         reducer,
         reducer_kwargs,
         window_size,
@@ -86,21 +87,21 @@ def train_ref_classify_rest(
         n_jobs=-1,
 ):
     """
-    Analyzes spike data using dimensionality reduction and classification.
+    Analyzes spike data using dimensionality reduction and regression.
 
     Parameters:
     - spks: The spike data.
     - bhv: Behavioral data containing masks and labels.
     - space_ref: Reference space identifier.
-    - classify: Column name in bhv to use for classification labels.
-    - classifier: Classifier to use.
-    - classifier_kwargs: Keyword arguments for the classifier.
+    - regress: Column name in bhv to use for regression labels.
+    - regressor: Regressor to use.
+    - regressor_kwargs: Keyword arguments for the regressor.
     - reducer: Dimensionality reduction method to use.
     - reducer_kwargs: Keyword arguments for the reducer.
     - window_size: Size of the window to use for analysis.
 
     Returns:
-    - Tuple of arrays (results_bl, results_f1) containing the balanced accuracy and F1 scores.
+    - Dictionary containing the mean squared error and R2 scores.
     """
     ref_spks = spks[bhv.mask == space_ref, :, :]
     nref_spks = spks[bhv.mask != space_ref, :, :]
@@ -117,38 +118,36 @@ def train_ref_classify_rest(
         ('reducer', reducer(**reducer_kwargs)),
     ])
 
-    y_ref = bhv[bhv.mask == space_ref][classify].values
-    y_nref = bhv[bhv.mask != space_ref][classify].values
+    y_ref = bhv[bhv.mask == space_ref][regress].values
+    y_nref = bhv[bhv.mask != space_ref][regress].values
     results_cv = Parallel(n_jobs=n_jobs, verbose=1)(
-        delayed(process_window)(w, ref_spks, nref_spks, window_size, y_ref, y_nref, reducer_pipeline, classifier,
-                                classifier_kwargs) for w in tqdm(range(ref_spks.shape[1] - window_size)))
+        delayed(process_window)(w, ref_spks, nref_spks, window_size, y_ref, y_nref, reducer_pipeline, regressor,
+                                regressor_kwargs) for w in tqdm(range(ref_spks.shape[1] - window_size)))
 
     results_perm = []
     if n_permutations > 0:
         for n in tqdm(range(n_permutations)):
             y_ref = np.random.permutation(y_ref)
             y_nref = np.random.permutation(y_nref)
-            # results_n = Parallel(n_jobs=n_jobs, verbose=1)(delayed(process_window)(w, ref_spks, nref_spks, window_size, y_ref, y_nref, reducer_pipeline, classifier, classifier_kwargs) for w in tqdm(range(ref_spks.shape[1] - window_size)))
-            # results_perm.append(results_n)
-            clf = DummyClassifier(strategy='stratified')
+            reg = DummyRegressor(strategy='mean')
             results_perm_n = []
             for w in tqdm(range(ref_spks.shape[1] - window_size)):
                 window_ref = ref_spks[:, w:w + window_size, :].reshape(ref_spks.shape[0], -1)
                 window_nref = nref_spks[:, w:w + window_size, :].reshape(nref_spks.shape[0], -1)
 
-                # Fit the classifier on the reference space
-                clf.fit(window_ref, y_ref)
+                # Fit the regressor on the reference space
+                reg.fit(window_ref, y_ref)
 
                 # Predict on the non-reference space
-                y_pred = clf.predict(window_nref)
+                y_pred = reg.predict(window_nref)
 
-                # Compute the accuracy
-                ba_score = balanced_accuracy_score(y_nref, y_pred)
-                f1_score_val = f1_score(y_nref, y_pred, average='weighted')
+                # Compute the mean squared error and R2 score
+                mse_score = mean_squared_error(y_nref, y_pred)
+                r2_score_val = r2_score(y_nref, y_pred)
 
                 results = {
-                    'ba_score': ba_score,
-                    'f1_score': f1_score_val,
+                    'mse_score': mse_score,
+                    'r2_score': r2_score_val,
                     'w': w,
                 }
 
@@ -159,86 +158,8 @@ def train_ref_classify_rest(
         'cv': results_cv,
         'perm': results_perm,
     }
-    # Unpack results
-    # results_bl, results_f1 = zip(*results)
 
     return results
-
-
-def train_within(
-        spks,
-        bhv,
-        skf,
-        space_ref,
-        classify,
-        classifier,
-        classifier_kwargs,
-        reducer,
-        reducer_kwargs,
-        window_size,
-        n_permutations=100,
-        n_jobs=-1,
-):
-    reducer_pipeline = Pipeline([
-        ('scaler', StandardScaler()),
-        ('reducer', reducer(**reducer_kwargs)),
-    ])
-
-    ref_spks = spks[bhv.mask == space_ref, :, :]
-    ref_labels = bhv[bhv.mask == space_ref][classify].values
-    cv_results = []
-    for i, (train_idx, test_idx) in enumerate(skf.split(ref_spks, ref_labels)):
-        print(f'Fold {i} / {skf.get_n_splits()}')
-        X_train = ref_spks[train_idx, :, :]
-        X_test = ref_spks[test_idx, :, :]
-        y_train = ref_labels[train_idx]
-        y_test = ref_labels[test_idx]
-
-        # Z-score to train data
-        spks_mean = np.nanmean(X_train, axis=0)
-        spks_std = np.nanstd(X_train, axis=0)
-        spks_std[spks_std == 0] = np.finfo(float).eps
-        X_train = (X_train - spks_mean) / spks_std
-        X_test = (X_test - spks_mean) / spks_std
-
-        results = Parallel(n_jobs=-1, verbose=1)(
-            delayed(process_window)(w, X_train, X_test, window_size, y_train, y_test, reducer_pipeline, classifier,
-                                    classifier_kwargs) for w in tqdm(range(ref_spks.shape[1] - window_size)))
-
-        cv_results.append(results)
-
-    results_perm = []
-    if n_permutations > 0:
-        for n in range(n_permutations):
-            print(f'Permutation {n} / {n_permutations}')
-            ref_labels = np.random.permutation(ref_labels)
-            for i, (train_idx, test_idx) in enumerate(skf.split(ref_spks, ref_labels)):
-                print(f'Fold {i} / {skf.get_n_splits()}')
-                X_train = ref_spks[train_idx, :, :]
-                X_test = ref_spks[test_idx, :, :]
-                y_train = ref_labels[train_idx]
-                y_test = ref_labels[test_idx]
-
-                # Z-score to train data
-                spks_mean = np.nanmean(X_train, axis=0)
-                spks_std = np.nanstd(X_train, axis=0)
-                spks_std[spks_std == 0] = np.finfo(float).eps
-                X_train = (X_train - spks_mean) / spks_std
-                X_test = (X_test - spks_mean) / spks_std
-
-                results = Parallel(n_jobs=-1, verbose=1)(
-                    delayed(process_window)(w, X_train, X_test, window_size, y_train, y_test, reducer_pipeline,
-                                            classifier, classifier_kwargs) for w in
-                    tqdm(range(ref_spks.shape[1] - window_size)))
-
-                results_perm.append(results)
-
-    results = {
-        'cv': cv_results,
-        'perm': results_perm,
-    }
-    return results
-
 
 def main():
     # Load and preprocess data here
@@ -339,8 +260,9 @@ def main():
         'n_jobs': 1,
     }
 
-    space_ref = ['No Noise', 'Noise']
-    regress = 'head_angle'  # Assuming 'head_angle' is the column in your DataFrame for regression
+    # space_ref = ['No Noise', 'Noise']
+    #temporarily remove the space_ref variable, I don't want to incorporate separate data yet
+    regress = 'dlc_angle'  # Assuming 'head_angle' is the column in your DataFrame for regression
 
     # Use KFold for regression
     kf = KFold(n_splits=5, shuffle=True)
@@ -354,33 +276,31 @@ def main():
     for run in range(n_runs):
         results_between[run] = {}
         results_within[run] = {}
-        for space in space_ref:
-            results_between[run][space] = train_ref_classify_rest(
-                spks,
-                bhv,
-                space,
-                regress,
-                regressor,
-                regressor_kwargs,
-                reducer,
-                reducer_kwargs,
-                window_size,
-                n_permutations=n_permutations,
-            )
+        # for space in space_ref:
+        results_between[run] = train_ref_classify_rest(
+            spks,
+            bhv,
+            regress,
+            regressor,
+            regressor_kwargs,
+            reducer,
+            reducer_kwargs,
+            window_size,
+            n_permutations=n_permutations,
+        )
 
-            results_within[run][space] = train_within(
-                spks,
-                bhv,
-                kf,
-                space,
-                regress,
-                regressor,
-                regressor_kwargs,
-                reducer,
-                reducer_kwargs,
-                window_size,
-                n_permutations=n_permutations,
-            )
+        results_within[run] = train_within(
+            spks,
+            bhv,
+            kf,
+            regress,
+            regressor,
+            regressor_kwargs,
+            reducer,
+            reducer_kwargs,
+            window_size,
+            n_permutations=n_permutations,
+        )
 
         # Save results
         results = {'between': results_between, 'within': results_within}
