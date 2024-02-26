f94c4687195d6a044e0e0a7435b0b3c2c4798698
carlacodes <carla.grifiths@gmail.com>
2024-02-26 18:03:43 +0000

    cleaning

--- run_umap_with_history_gridsearch.py
+++ run_umap_with_history_gridsearch.py
@@ -20,6 +20,7 @@ from sklearn.model_selection import RepeatedStratifiedKFold, StratifiedKFold, Ti
 from sklearn.svm import SVC
 from sklearn.metrics import balanced_accuracy_score, f1_score
 from sklearn.dummy import DummyClassifier
+from pathlib import Path
 from sklearn.metrics import mean_squared_error, r2_score
 from umap import UMAP
 from sklearn.model_selection import train_test_split
@@ -96,7 +97,6 @@ def train_and_test_on_reduced(
         reducer,
         reducer_kwargs,
         window_size,
-        n_permutations=100,
         n_jobs_parallel=1,
 ):
     # Define the grid of hyperparameters
@@ -104,7 +104,7 @@ def train_and_test_on_reduced(
         'regressor__kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
         'regressor__C': [0.1, 1, 10],
         'reducer__n_components': [2, 3, 4],
-        'reducer__n_neighbors': [10, 20, 30, 40, 50],
+        'reducer__n_neighbors': [10, 20, 30, 40, 50, 60, 70, 80],
         'reducer__min_dist': [0.1, 0.2, 0.3, 0.4, 0.5],
         'reducer__metric': ['euclidean', 'manhattan', 'chebyshev', 'minkowski'],
     }
@@ -167,116 +167,16 @@ def train_and_test_on_reduced(
     # After the loop, the best hyperparameters are those that yield the largest difference
     return best_params
 
-def train_and_test_on_reduced(
-        spks,
-        bhv,
-        regress,
-        regressor,
-        regressor_kwargs,
-        reducer,
-        reducer_kwargs,
-        window_size,
-        n_permutations=100,
-        n_jobs_parallel=1,
-):
-
-
-    reducer_pipeline = Pipeline([
-        #not sure if scaler is actually needed here TODO: check if scaler is needed
-        # ('scaler', StandardScaler()),
-        ('reducer', reducer(**reducer_kwargs)),
-    ])
-    y = bhv[regress].values
-
-
-    cv_results = []
-    skf = TimeSeriesSplit(n_splits=5)
-
-    for i, (train_idx, test_idx) in enumerate(skf.split(spks, y)):
-        print(f'Fold {i} / {skf.get_n_splits()}')
-        X_train = spks[train_idx, :, :]
-        X_test = spks[test_idx, :, :]
-        y_train = y[train_idx]
-        y_test = y[test_idx]
-
-        # Z-score to train data
-        spks_mean = np.nanmean(X_train, axis=0)
-
-        spks_std = np.nanstd(X_train, axis=0)
-        spks_std[spks_std == 0] = np.finfo(float).eps
-
-
-
-        X_train = (X_train - spks_mean) / spks_std
-        X_test = (X_test - spks_mean) / spks_std
-
-        # results = Parallel(n_jobs=n_jobs_parallel, verbose=1)(
-        #     delayed(process_window_within_split)(w, X_train, X_test, window_size, y_train, y_test, reducer_pipeline, regressor,
-        #                                          regressor_kwargs) for w in tqdm(range(spks.shape[1] - window_size)))
-        #possible redundnacy in Jules' original code as he normalises the data and then ALSO applies a Scaler, seems a bit redundant
-        results = Parallel(n_jobs=n_jobs_parallel, backend='loky', verbose=1)(
-            delayed(process_window_within_split)(
-                w, X_train, X_test, window_size, y_train, y_test, reducer_pipeline, regressor, regressor_kwargs
-            ) for w in tqdm(range(spks.shape[1] - window_size))
-        )
-
-        cv_results.append(results)
-
-    results_perm = []
-    if n_permutations > 0:
-        for n in range(n_permutations):
-            print(f'Permutation {n} / {n_permutations}')
-            y_perm = np.random.permutation(y)
-            spks_perm = np.random.permutation(spks)
-            for i, (train_idx, test_idx) in enumerate(skf.split(spks_perm, y_perm)):
-                print(f'Fold {i} / {skf.get_n_splits()}')
-                X_train = spks_perm[train_idx, :, :]
-                X_test = spks_perm[test_idx, :, :]
-                y_train = y_perm[train_idx]
-                y_test = y_perm[test_idx]
-
-                # Z-score to train data
-                spks_mean = np.nanmean(X_train, axis=0)
-                spks_std = np.nanstd(X_train, axis=0)
-                spks_std[spks_std == 0] = np.finfo(float).eps
-
-
-                X_train = (X_train - spks_mean) / spks_std
-                X_test = (X_test - spks_mean) / spks_std
-
-                results = Parallel(n_jobs=n_jobs_parallel, verbose=1)(
-                    delayed(process_window_within_split)(w, X_train, X_test, window_size, y_train, y_test, reducer_pipeline,
-                                            regressor, regressor_kwargs) for w in
-                    tqdm(range(spks.shape[1] - window_size)))
-
-                results_perm.append(results)
-
-    results = {
-        'cv': cv_results,
-        'perm': results_perm,
-    }
-    return results
 
 def main():
     data_dir = 'C:/neural_data/rat_7/6-12-2019/'
-    results_old = np.load(f'{data_dir}/results_2024-02-22_16-57-58.npy', allow_pickle=True).item(0)
-
-    # data_dir = '/media/jake/DataStorage_6TB/DATA/neural_network/og_honeycomb/rat7/6-12-2019'
-
-    # load spike data
-    # spike_dir = os.path.join(data_dir, 'spike_sorting')
-    # units = load_pickle('units_w_behav_correlates', spike_dir)
-
     spike_dir = os.path.join(data_dir, 'physiology_data')
     # spike_trains = load_pickle('spike_trains', spike_dir)
     dlc_dir = os.path.join(data_dir, 'positional_data')
 
-
     #load labels
     labels = np.load(f'{dlc_dir}/labels.npy')
     spike_data = np.load(f'{spike_dir}/inputs.npy')
-
-
 
     bins_before = 6  # How many bins of neural data prior to the output are used for decoding
     bins_current = 1  # Whether to use concurrent time bin of neural data
@@ -286,10 +186,8 @@ def main():
     X_for_umap = X[6:-6]
     labels_for_umap = labels[6:-6]
     labels_for_umap = labels_for_umap[:, 0:3]
-    # labels_for_umap = labels[:, 0:3]
     label_df = pd.DataFrame(labels_for_umap, columns=['x', 'y', 'angle'])
     label_df['time_index'] = np.arange(0, label_df.shape[0])
-    # unsupervised_umap(X_for_umap, label_df, remove_low_variance_neurons=False, n_components=3)
     bin_width = 1
     window_for_decoding = 6  # in s
     window_size = int(window_for_decoding / bin_width)  # in bins
@@ -318,16 +216,14 @@ def main():
     # kf = KFold(n_splits=5, shuffle=True)
 
     now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    filename = f'results_{now}.npy'
-    results_between = {}
+    filename = f'params_{now}.npy'
+    param_results = {}
     results_within = {}
     results_w_perm_reduced = {}
     n_permutations = 5
     for run in range(n_runs):
-        results_between[run] = {}
-        results_within[run] = {}
-        results_w_perm_reduced[run] = train_and_test_on_reduced(
-            X_for_umap,
+        best_params = train_and_test_on_reduced(
+            X,
             label_df,
             regress,
             regressor,
@@ -335,14 +231,12 @@ def main():
             reducer,
             reducer_kwargs,
             window_size,
-            n_permutations=n_permutations, n_jobs_parallel=8
+            n_jobs_parallel=1,
         )
+        param_results[run] = best_params
+    #save the results
+    np.save(filename, param_results)
 
-        # Save results
-    results = {'between': results_between, 'within': results_w_perm_reduced}
-    save_path = Path('C:/neural_data/rat_7/6-12-2019')
-    save_path.mkdir(exist_ok=True)
-    np.save(save_path / filename, results)
 
 
 if __name__ == '__main__':
