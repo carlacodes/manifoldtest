a6049cfa9b86a07863312c48e26783bfcb366fc3
carlacodes <carla.grifiths@gmail.com>
2024-02-27 22:21:23 +0000

    n_jobs was 16 -- changing to 8 for gridsearch

--- run_umap_with_history_gridsearch.py
+++ run_umap_with_history_gridsearch.py
@@ -245,7 +245,7 @@ def main():
             reducer,
             reducer_kwargs,
             window_size,
-            n_jobs_parallel=16,
+            n_jobs_parallel=8,
         )
         if diff_result > largest_diff:
             largest_diff = diff_result
