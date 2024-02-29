983c0c8b8d96ee6350f771bbfbf9e156e9fd0cc0
carlacodes <carla.grifiths@gmail.com>
2024-02-29 12:48:49 +0000

    adding dropout of 0.2

--- run_lstm_with_history.py
+++ run_lstm_with_history.py
@@ -23,10 +23,11 @@ from torch import nn
 
 # Define LSTM model
 class LSTMNet(nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim):
+    def __init__(self, input_dim, hidden_dim, output_dim, dropout_prob=0.2):
         super(LSTMNet, self).__init__()
         self.hidden_dim = hidden_dim
         self.lstm = nn.LSTM(input_dim, hidden_dim, batch_first=True)
+        self.dropout = nn.Dropout(dropout_prob)
         self.fc = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, x):
