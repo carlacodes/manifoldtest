e7efab4dcc79e01e0d10e628f71399afa93f07bf
carlacodes <carla.grifiths@gmail.com>
2024-03-01 15:57:11 +0000

    predicting both x and y

--- run_lstm_with_history_x_y.py
+++ run_lstm_with_history_x_y.py
@@ -127,21 +127,26 @@ def run_lstm_with_history(data_dir):
     # labels_for_umap = labels[:, 0:3]
     label_df = pd.DataFrame(labels_for_umap, columns=['x', 'y', 'angle_sin', 'angle_cos', 'dlc_angle_raw'])
     label_df['time_index'] = np.arange(0, label_df.shape[0])
-    target_label = 'x'
-    target = label_df[target_label].values
+    target_label = 'xy'
+    target_x = label_df['x'].values
+    target_y = label_df['y'].values
+
+    # Stack 'x' and 'y' to form a 2D target array
+    target = np.column_stack((target_x, target_y))
 
     #big dataframe
     big_score_df = pd.DataFrame()
     #isolate each of the 112 neurons and run the lstm on each of them
     for i in range(0, X_for_lstm.shape[2]):
         X_of_neuron = X_for_lstm[:, :, i]
-        target_reshaped = target.reshape(-1, 1)
+        target_reshaped = target.reshape(-1, 2)  # reshape to (-1, 2)
 
-        #add back the third dimension
+        # add back the third dimension
         X_of_neuron = X_of_neuron.reshape(X_of_neuron.shape[0], X_of_neuron.shape[1], 1)
         score_df_neuron = run_lstm(X_of_neuron, target_reshaped)
         score_df_neuron['neuron_index'] = i
         big_score_df = pd.concat([big_score_df, score_df_neuron], ignore_index=True)
+
     #save big_score_df to csv
     big_score_df.to_csv(f'{data_dir}/lstm_scores_{target_label}.csv')
     print('done')
