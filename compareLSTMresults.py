cc6ddd5af0fdaee57c0f7dcd06d93cec3098c57a
carlacodes <carla.grifiths@gmail.com>
2024-03-04 11:39:10 +0000

    adding script to compareLSTMresults.py

--- compareLSTMresults.py
+++ compareLSTMresults.py
@@ -24,9 +24,9 @@ def load_csvs(csv_dir):
 def run_analysis_on_csvs(csv_dict):
     #get the number of neurons that have scores that are above the mean_perm_score
     big_df_by_independent_variable = pd.DataFrame()
-    for i in csv_dict.keys():
+    for key in csv_dict.keys():
         #get the specific dataframe
-        df = csv_dict[i]
+        df = csv_dict[key]
         #get the number of unique neuron idices
         unique_neuron_indices = df['neuron_index'].unique()
         for i in unique_neuron_indices:
@@ -40,17 +40,17 @@ def run_analysis_on_csvs(csv_dict):
             print(f'Neuron {i} has {fraction_above_mean_perm_score} fraction of scores that are above the mean_perm_score')
             #add to a separate dataframe
             if i == 0:
-                fraction_df = pd.DataFrame({'neuron_index': [i], 'fraction_above_mean_perm_score': [fraction_above_mean_perm_score]})
+                fraction_df = pd.DataFrame({'neuron_index': [i], 'fraction_above_mean_perm_score': [fraction_above_mean_perm_score], 'independent_variable': [key]})
             else:
-                current_fraction_df = pd.DataFrame({'neuron_index': [i], 'fraction_above_mean_perm_score': [fraction_above_mean_perm_score]})
+                current_fraction_df = pd.DataFrame({'neuron_index': [i], 'fraction_above_mean_perm_score': [fraction_above_mean_perm_score], 'independent_variable': [key]})
                 fraction_df = pd.concat([fraction_df, current_fraction_df], ignore_index=True)
         #add to the big dataframe
-        fraction_df['independent_variable'] = i
         big_df_by_independent_variable = pd.concat([big_df_by_independent_variable, fraction_df], ignore_index=True)
     return big_df_by_independent_variable
 
 def main():
     csv_dict = load_csvs('C:/neural_data/rat_7/lstm_csvs/')
+    big_df = run_analysis_on_csvs(csv_dict)
 
 
 
