1882e49b0ca4d43fd075358a76fb757a4ebb8c6f
carlacodes <carla.grifiths@gmail.com>
2024-03-05 11:23:37 +0000

    plotting the differences

--- compareLSTMresults.py
+++ compareLSTMresults.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
@@ -25,6 +26,7 @@ def load_csvs(csv_dir):
 def run_analysis_on_csvs(csv_dict):
     #get the number of neurons that have scores that are above the mean_perm_score
     big_df_by_independent_variable = pd.DataFrame()
+    big_df_by_independent_variable_diff = pd.DataFrame()
     for key in csv_dict.keys():
         #get the specific dataframe
         df = csv_dict[key]
@@ -34,6 +36,10 @@ def run_analysis_on_csvs(csv_dict):
             #get the scores of the current neuron
             current_neuron_scores = df[df['neuron_index'] == i]
             #get the number of scores that are above the mean_perm_score
+            difference_in_scores = current_neuron_scores['score'] - current_neuron_scores['mean_perm_score']
+            difference_in_scores = np.array(difference_in_scores)
+            neuron_index = np.array([i]*len(difference_in_scores))
+            key_index = np.array([key]*len(difference_in_scores))
             scores_above_mean_perm_score = current_neuron_scores[current_neuron_scores['score'] > current_neuron_scores['mean_perm_score']]
             print(f'Neuron {i} has {len(scores_above_mean_perm_score)} scores that are above the mean_perm_score')
             #get the fraction of scores that are above the mean_perm_score
@@ -42,12 +48,19 @@ def run_analysis_on_csvs(csv_dict):
             #add to a separate dataframe
             if i == 0:
                 fraction_df = pd.DataFrame({'neuron_index': [i], 'fraction_above_mean_perm_score': [fraction_above_mean_perm_score], 'independent_variable': [key]})
+                diff_df = pd.DataFrame({'neuron_index': neuron_index, 'difference_in_scores': difference_in_scores, 'independent_variable': key_index})
             else:
                 current_fraction_df = pd.DataFrame({'neuron_index': [i], 'fraction_above_mean_perm_score': [fraction_above_mean_perm_score], 'independent_variable': [key]})
+                current_diff_df = pd.DataFrame({'neuron_index': neuron_index, 'difference_in_scores': difference_in_scores, 'independent_variable': key_index})
+
+
+
                 fraction_df = pd.concat([fraction_df, current_fraction_df], ignore_index=True)
+                diff_df = pd.concat([diff_df, current_diff_df], ignore_index=True)
         #add to the big dataframe
         big_df_by_independent_variable = pd.concat([big_df_by_independent_variable, fraction_df], ignore_index=True)
-    return big_df_by_independent_variable
+        big_df_by_independent_variable_diff = pd.concat([big_df_by_independent_variable_diff, diff_df], ignore_index=True)
+    return big_df_by_independent_variable, big_df_by_independent_variable_diff
 
 def plot_histograms(big_df_by_independent_variable):
     fig, ax = plt.subplots(1, 3, figsize=(15, 5))
@@ -64,10 +77,29 @@ def plot_histograms(big_df_by_independent_variable):
     plt.savefig('C:/neural_data/rat_7/lstm_csvs/histograms.png')
     plt.show()
 
+def plot_difference_in_scores(dataframe):
+    fig, ax = plt.subplots(1, 3, figsize=(15, 5))
+    #plot the histograms
+    for i in range(0, len(dataframe['independent_variable'].unique())):
+        current_independent_variable = dataframe['independent_variable'].unique()[i]
+        current_df = dataframe[dataframe['independent_variable'] == current_independent_variable]
+        ax[i].hist(current_df['difference_in_scores'])
+        ax[i].set_ylim(0, 40)
+        ax[i].set_title(f'{current_independent_variable}')
+        ax[i].set_xlabel('Difference in scores')
+        ax[i].set_ylabel('Number of neurons')
+    plt.savefig('C:/neural_data/rat_7/lstm_csvs/histograms_diff.png', dpi = 300, bbox_inches = 'tight')
+    plt.show()
+
+    return
+
+
 def main():
     csv_dict = load_csvs('C:/neural_data/rat_7/lstm_csvs/')
-    big_df = run_analysis_on_csvs(csv_dict)
+    big_df, diff_df = run_analysis_on_csvs(csv_dict)
     plot_histograms(big_df)
+    plot_difference_in_scores(diff_df)
+    #plot the distribution of scores - mean perm score for each neuron
 
 
 
