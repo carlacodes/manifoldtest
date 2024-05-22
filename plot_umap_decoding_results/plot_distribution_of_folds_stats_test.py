fixing normalisation to scale between 0 and 1"

plot_umap_decoding_results/plot_distribution_of_folds_stats_test.py
@@ -969,10 +969,10 @@ def run_ks_test_on_distributions_3d_grid(data_dir, param_dict, score_dict, big_d
                     #convert train_grouped and test_grouped to an array for the ks test
                     train_grouped_for_testing = train_grouped.values
                     #normalise
-                    train_grouped_for_testing = train_grouped_for_testing / np.sum(train_grouped_for_testing)
+                    train_grouped_for_testing = train_grouped_for_testing / np.max(train_grouped_for_testing)
 
                     test_grouped_for_testing = test_grouped.values
-                    test_grouped_for_testing = test_grouped_for_testing / np.sum(test_grouped_for_testing)
+                    test_grouped_for_testing = test_grouped_for_testing / np.max(test_grouped_for_testing)
                     # fig,ax = plt.subplots(1, 1)
                     # ax.plot(train_grouped_for_testing, label='train')
                     # ax.plot(test_grouped_for_testing, label='test')
@@ -1072,6 +1072,7 @@ def run_ks_test_on_distributions_3d_grid(data_dir, param_dict, score_dict, big_d
 
 
     np.unique(df_across_windows['mean_minimum_number_windows_across_rats_and_windowsize'])
+    np.unique(df_across_windows['mean_minimum_number_windows_by_windowsize'])
 
     # export to csv
     df_across_windows.to_csv(f'{big_df_savedir}/mean_p_value_vs_window_size_across_rats_grid.csv')