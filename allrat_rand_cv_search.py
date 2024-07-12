8375ac064d2e376bf48990f7f1fd61dae44a45f9
carlacodes <carla.griffiths@gmail.com>
2024-07-12 13:23:51 +0100

    adding standardsclaer to sklearn pipeline

--- allrat_rand_cv_search.py
+++ allrat_rand_cv_search.py
@@ -1,34 +1,40 @@
 # from pathlib import Path
 import copy
 from datetime import datetime
-from sklearn.model_selection import ParameterSampler
 from sklearn.multioutput import MultiOutputRegressor
 import matplotlib.pyplot as plt
-from sklearn.pipeline import Pipeline
-# from helpers.datahandling import DataHandler
-from scipy.stats import randint
 from sklearn.neighbors import KNeighborsRegressor
 from pathlib import Path
-from sklearn.metrics import mean_squared_error, r2_score
 from umap import UMAP
-from sklearn.decomposition import PCA
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.pipeline import Pipeline
-from skopt import BayesSearchCV
-from sklearn.pipeline import Pipeline
 import logging
 import sys
-
 ''' Modified from Jules Lebert's code
 spks was a numpy arrray of size trial* timebins*neuron, and bhv is  a pandas dataframe where each row represents a trial, the trial is the index '''
 import os
-import scipy
-import pickle as pkl
 from sklearn.base import BaseEstimator
+from sklearn.model_selection import BaseCrossValidator
+from sklearn.preprocessing import StandardScaler
+import numpy as np
 
+class ZScoreCV(BaseCrossValidator):
+    def __init__(self, spks, custom_folds):
+        self.spks = spks
+        self.custom_folds = custom_folds
 
+    def split(self, X, y=None, groups=None):
+        for train_idx, test_idx in self.custom_folds:
+            scaler = StandardScaler()
+            # Fit on training data and transform both training and testing data
+            self.spks[train_idx] = scaler.fit_transform(self.spks[train_idx])
+            self.spks[test_idx] = scaler.transform(self.spks[test_idx])
+            yield train_idx, test_idx
+
+    def get_n_splits(self, X=None, y=None, groups=None):
+        return len(self.custom_folds)
 class CustomUMAP(BaseEstimator):
     def __init__(self, n_neighbors=15, n_components=2, metric='euclidean',
                  n_epochs=None, learning_rate=1.0, init='spectral',
@@ -148,15 +154,6 @@ def train_and_test_on_umap_randcv(
     sys.stdout = log_file
 
     if use_rand_search:
-        # Define the parameter grid
-        # param_grid = {
-        #     'estimator__estimator__n_neighbors': (2, 70),  # range of values
-        #     'reducer__n_components': (3, 10),
-        #     'estimator__estimator__metric': ['euclidean', 'cosine', 'minkowski'],
-        #     'reducer__n_neighbors': (2, 70),  # range of values
-        #     'reducer__min_dist': (0.0001, 0.3),  # range of values
-        #     'reducer__random_state': [42]
-        # }
         param_grid = {
            'reducer__n_components': [3, 4, 5, 6, 7, 8, 9, 10],
             'reducer__min_dist': [0.0001, 0.001, 0.01, 0.1, 0.3],
@@ -164,13 +161,14 @@ def train_and_test_on_umap_randcv(
             'reducer__random_state': [42],
             'estimator__estimator__n_neighbors': [2, 5, 10, 20, 30, 40, 50, 60, 70],
            'estimator__estimator__metric': ['cosine', 'euclidean', 'minkowski'],}
+        zscore_cv = ZScoreCV(spks, custom_folds)
 
         # Initialize BayesSearchCV
         random_search = RandomizedSearchCV(
             pipeline,
             param_distributions=param_grid,
-            n_iter=200,
-            cv=custom_folds,
+            n_iter=500,
+            cv=zscore_cv,
             verbose=3,
             n_jobs=-1,
             scoring='r2'
@@ -185,19 +183,18 @@ def train_and_test_on_umap_randcv(
         best_params = random_search.best_params_
         best_score = random_search.best_score_
     else:
-        # Manually set the parameters
 
-        # Initialize lists to store the scores
         train_scores = []
         test_scores = []
 
-        # Loop over the custom folds
         count = 0
         for train_index, test_index in custom_folds:
             # Split the data into training and testing sets
             spks_train, spks_test = spks[train_index], spks[test_index]
             y_train, y_test = y[train_index], y[test_index]
-
+            scaler = StandardScaler()
+            spks_train_scaled = scaler.fit_transform(spks_train)
+            spks_test_scaled = scaler.transform(spks_test)
             # Set the parameters
             formatted_params = format_params(manual_params)
             pipeline.set_params(**formatted_params)
