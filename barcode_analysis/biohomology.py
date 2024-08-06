a9fa2b24649250e0b0c010f423bf3150d0b7f777
carlacodes <carla.griffiths@gmail.com>
2024-08-06 14:45:52 +0100

    cleaning up commented out code

--- barcode_analysis/biohomology.py
+++ barcode_analysis/biohomology.py
@@ -14,6 +14,8 @@ import seaborn as sns
 import numpy as np
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
+from scipy.interpolate import UnivariateSpline
+from helpers import utils
 
 def plot_homology_changes_heatmap_interval(dgm_dict, save_dir, start_indices, end_indices):
     """
@@ -146,24 +148,6 @@ def plot_homology_changes_heatmap(dgm_dict, save_dir, start_indices, end_indices
     return df
 
 
-def calculate_goodness_of_fit(x_data, y_data, params):
-    # Calculate the fitted values
-    y_fitted = sinusoidal(x_data, *params)
-
-    # Calculate residuals
-    residuals = y_data - y_fitted
-
-    # Sum of squared residuals (SSR)
-    ssr = np.sum(residuals ** 2)
-
-    # Total sum of squares (SST)
-    sst = np.sum((y_data - np.mean(y_data)) ** 2)
-
-    # R-squared
-    r_squared = 1 - (ssr / sst)
-
-    return r_squared
-
 def sinusoidal(x, A, B, C, D):
     return A * np.sin(B * x + C) + D
 
@@ -189,12 +173,13 @@ def fit_sinusoid_data(df, save_dir):
         params, _ = curve_fit(sinusoidal, x_data, y_data, p0=initial_guess)
         fit_params[dim] = params
         r_squared = calculate_goodness_of_fit(x_data, y_data, params)
+
         print(f'R-squared for dimension {dim}: {r_squared}')
 
         # Plot the original data and the fitted function
         plt.figure(figsize=(10, 6))
         plt.plot(x_data, y_data, 'bo', label='Original Data')
-        plt.plot(x_data, sinusoidal(x_data, *params), 'r-', label='Fitted Function')
+        # plt.plot(x_data, sinusoidal(x_data, *params), 'r-', label='Fitted Function')
         plt.title(f'Sinusoidal Fit for Homology Dimension {dim}')
         plt.xlabel('Interval (j)')
         plt.ylabel('Mean Death - Birth')
@@ -208,6 +193,82 @@ def fit_sinusoid_data(df, save_dir):
     return fit_params
 
 
+
+def fit_smooth_function(df, save_dir):
+    """
+    Fit a smooth function to the data and plot the results.
+
+    Parameters
+    ----------
+    df: pd.DataFrame
+        DataFrame containing the data to fit.
+    save_dir: str
+        Directory to save the plot.
+    """
+    # Extract data from the heatmap
+    heatmap_data = df.pivot_table(index='Interval', columns='Dimension', values='death_minus_birth', aggfunc='mean')
+
+    # Prepare to store fit parameters
+    fit_params = {}
+
+    # Fit the function for each homology dimension
+    for dim in heatmap_data.columns:
+        x_data = heatmap_data.index.values
+        y_data = heatmap_data[dim].values
+
+        # Fit a smooth spline to the data
+        spline = UnivariateSpline(x_data, y_data, s=1)
+        fit_params[dim] = spline
+        r_squared = calculate_goodness_of_fit(x_data, y_data, spline(x_data))
+        print(f'R-squared for dimension {dim}: {r_squared}')
+
+        # Plot the original data and the fitted function
+        plt.figure(figsize=(10, 6))
+        plt.plot(x_data, y_data, 'bo', label='Original Data')
+        plt.plot(x_data, spline(x_data), 'r-', label='Fitted Function')
+        plt.title(f'Smooth Fit for Homology Dimension {dim}')
+        plt.xlabel('Interval (j)')
+        plt.ylabel('Mean Death - Birth')
+        plt.legend()
+        plt.show()
+
+    # Print the fit parameters for each dimension
+    for dim, spline in fit_params.items():
+        print(f'Dimension {dim}: Spline knots={spline.get_knots()}')
+
+    return fit_params
+
+def calculate_goodness_of_fit(x_data, y_data, y_fitted):
+    """
+    Calculate the goodness of fit (R-squared) for the fitted function.
+
+    Parameters
+    ----------
+    x_data: np.array
+        Array of x data points.
+    y_data: np.array
+        Array of y data points.
+    y_fitted: np.array
+        Array of fitted y data points.
+
+    Returns
+    -------
+    float
+        R-squared value.
+    """
+    # Calculate residuals
+    residuals = y_data - y_fitted
+
+    # Sum of squared residuals (SSR)
+    ssr = np.sum(residuals ** 2)
+
+    # Total sum of squares (SST)
+    sst = np.sum((y_data - np.mean(y_data)) ** 2)
+
+    # R-squared
+    r_squared = 1 - (ssr / sst)
+
+    return r_squared
 def plot_homology_changes_over_j(dgm_dict, save_dir):
     """
     Plot how the homology changes over the range of `j`.
@@ -307,7 +368,7 @@ def plot_barcode(diag, dim, save_dir=None,fold = 0, **kwargs):
 
 
 
-def run_persistence_analysis(folder_str, input_df, use_ripser=False):
+def run_persistence_analysis(folder_str, input_df, use_ripser=False, segment_length=40):
     pairs_list = []
     dgm_dict = {}
     sorted_list = []
@@ -316,13 +377,13 @@ def run_persistence_analysis(folder_str, input_df, use_ripser=False):
     trial_info = input_df
     trial_indices = trial_info['trial']
 
-    for i in range(int(len(reduced_data) / 40)):
-        reduced_data_loop = reduced_data[i * 40:(i + 1) * 40]
-        if len(set(trial_indices[i * 40:(i + 1) * 40])) == 1:
-            sorted_list.append(list(range(i * 40, (i + 1) * 40)))
+    for i in range(int(len(reduced_data) / segment_length)):
+        reduced_data_loop = reduced_data[i * segment_length:(i + 1) * segment_length]
+        if len(set(trial_indices[i * segment_length:(i + 1) * segment_length])) == 1:
+            sorted_list.append(list(range(i * segment_length, (i + 1) * segment_length)))
         else:
             subdivided_indices = []
-            for k, g in groupby(enumerate(range(i * 40, (i + 1) * 40)), lambda ix: ix[0] - ix[1]):
+            for k, g in groupby(enumerate(range(i * segment_length, (i + 1) * segment_length)), lambda ix: ix[0] - ix[1]):
                 continuous_segment = list(map(itemgetter(1), g))
                 trial_subdivisions = []
                 for k, g in groupby(continuous_segment, key=lambda x: trial_indices[x]):
@@ -366,7 +427,7 @@ def run_persistence_analysis(folder_str, input_df, use_ripser=False):
             np.save(folder_str + '/dgm_fold_h2' + '_interval_' + str(j) + '.npy', dgm)
     #generate the trial indices where the trial changes
     df_output = plot_homology_changes_heatmap(dgm_dict, folder_str, start_intervals, end_intervals)
-    fit_params = fit_sinusoid_data(df_output, folder_str)
+    fit_params = utils.fit_sinusoid_data_per_interval(df_output, folder_str, start_intervals, end_intervals)
 
     if use_ripser:
         with open(folder_str + '/pairs_list_h2.pkl', 'wb') as f:
@@ -376,186 +437,6 @@ def run_persistence_analysis(folder_str, input_df, use_ripser=False):
             pickle.dump(dgm_dict, f)
 
     return pairs_list
-
-# def run_persistence_analysis(folder_str, input_df, use_ripser=False):
-#     pairs_list = []
-#     dgm_dict = {}
-#     sorted_list = []
-#
-#
-#     reduced_data = (
-#         np.load(folder_str + '/full_set_transformed.npy'))
-#     #import the folds
-#     # folds_data = pd.read_csv(folder_str + '/custom_folds.csv')
-#     # #load the test indices
-#     # fold_data = folds_data['train'][i]
-#     # #convert this to an int array
-#     # #remove the first bracket and the last bracket
-#     # fold_data = fold_data.strip('[]')  # Remove brackets
-#     #
-#     # fold_data = np.array(fold_data.split(',')).astype(int)
-#     # #load the corresponding trial info
-#     trial_info = input_df
-#     trial_indices = trial_info['trial']
-#     #sort the data into segments of 40 smamples and check they are all part of the same trial
-#
-#
-#     for i in range(int(len(reduced_data)/ 40)):
-#         reduced_data_loop = reduced_data[i*40:(i+1)*40]
-#         #check they are all part of the same trial
-#         if len(set(trial_indices[i*40:(i+1)*40])) == 1:
-#             sorted_list.append(list(range(i * 40, (i + 1) * 40)))
-#         else:
-#             #subdivide the segments into continuous segments
-#             subdivided_indices = []
-#             for k, g in groupby(enumerate(range(i * 40, (i + 1) * 40)), lambda ix: ix[0] - ix[1]):
-#                 continuous_segment = list(map(itemgetter(1), g))
-#                 # Further subdivide by trial indices
-#                 trial_subdivisions = []
-#                 for k, g in groupby(continuous_segment, key=lambda x: trial_indices[x]):
-#                     if len(list(g)) >= 20:
-#                         trial_subdivisions.append(list(g))
-#                 subdivided_indices.extend(trial_subdivisions)
-#             sorted_list.extend(subdivided_indices)
-#
-#     #remove the empty lists
-#     sorted_list = [x for x in sorted_list if x != []]
-#
-#
-#
-#     for j in range(len(sorted_list)):
-#         reduced_data_loop = reduced_data[sorted_list[j], :]
-#         if use_ripser:
-#             pairs = rpp.run("--format point-cloud --dim " + str(2), reduced_data_loop)[2]
-#             print('pairs shape', pairs.shape)
-#             #append pairs to a list
-#             pairs_list.append(pairs)
-#
-#             #
-#             #plot th persistence as a scatter plot
-#             fig, ax = plt.subplots(1, 3, figsize=(15, 5))
-#             flattened_pairs = pairs.flatten()
-#             flattened_pairs = flattened_pairs[0]
-#             pairs_birth = pairs['birth']
-#
-#             plt.scatter(pairs['birth'], pairs['death'])
-#             plt.xlabel('birth')
-#             plt.ylabel('death')
-#             plt.title('Persistence scatter plot')
-#             # plt.show()
-#             #save the individual pairs with the count
-#             np.save(folder_str + '/pairs_fold_h2' + str(j) + '.npy', pairs)
-#         else:
-#             dgm = ripser_parallel(reduced_data_loop, maxdim=2, n_threads=20, return_generators=True)
-#             dgm_gtda = _postprocess_diagrams([dgm["dgms"]], "ripser", (0, 1, 2), np.inf, True)
-#             diagram = plot_diagram(dgm_gtda[0], homology_dimensions=(0, 1,2))
-#             # diagram.show()
-#             diagram.write_html(folder_str + '/dgm_fold_h2_fold'+ '_interval_' + str(j) + '.html')
-#             dgm_dict[i] = dgm
-#             #plot the betti curve using the giotto package
-#             betti_curve_transformer = BettiCurve(n_bins=1000, n_jobs=20)  # n_bins controls the resolution of the Betti curve
-#             betti_curves = betti_curve_transformer.fit_transform(dgm_gtda)
-#             fig = betti_curve_transformer.plot(betti_curves, sample=0)
-#             #save plotly object figure
-#             fig.write_html(folder_str + '/betti_curve_fold_h2_fold' + '_interval_' + str(j) + '.html')
-#             #save the individual persistence diagrams
-#             #subtract the first dimension from the second dimension
-#             dgm_gtda_difference = dgm_gtda[0][:,1] - dgm_gtda[0][:,0]
-#             dgm_gtda_copy = copy.deepcopy(dgm_gtda[0])
-#             dgm_gtda_difference = dgm_gtda_difference.reshape(-1, 1)
-#
-#             dgm_gtda_copy = np.hstack((dgm_gtda_copy, dgm_gtda_difference))
-#             #convert to a dataframe
-#             dgm_gtda_df = pd.DataFrame(dgm_gtda_copy, columns=['birth', 'death', 'dim', 'difference'])
-#             #filter for when difference is greater than 0
-#             dgm_gtda_df_filtered = dgm_gtda_df[dgm_gtda_df['difference'] >= 0.2]
-#             #plot the barcode for the filtered data, where the y axis represents the dimension
-#             plt.figure(figsize=(10, 6))
-#             # Define the vertical offset for staggering the 0-dimensional bars
-#             offset = 0.1
-#             dimension_1_base = int(len(dgm_gtda_df_filtered[dgm_gtda_df_filtered['dim'] == 0])*0.1)+10
-#             dimension_2_base = int(len(dgm_gtda_df_filtered[dgm_gtda_df_filtered['dim'] == 1])*0.1)+10 + dimension_1_base
-#             # Set a base y-position for dimension 1 to ensure it is above dimension 0
-#
-#             # Initialize a dictionary to keep track of the current offset for each homology dimension
-#             current_offsets = {}
-#
-#             # Prepare a list to store all y-positions for setting ticks later
-#             y_positions = []
-#
-#             # Plot each bar in the barcode
-#             for index, row in dgm_gtda_df_filtered.iterrows():
-#                 birth = row['birth']
-#                 death = row['death']
-#                 dimension = int(row['dim'])  # Convert dimension to integer if it's not already
-#
-#                 # Determine the y-position for the bar
-#                 if dimension == 0:
-#                     # If dimension is 0, apply the staggered offset
-#                     if dimension not in current_offsets:
-#                         current_offsets[dimension] = 0  # Initialize the offset for dimension 0
-#                     y_position = dimension + current_offsets[dimension]
-#                     current_offsets[dimension] += offset  # Increment the offset for the next bar
-#                     color_txt = 'g-'
-#                 elif dimension == 1:
-#                     # Ensure dimension 1 starts above the max of dimension 0
-#                     y_position = dimension_1_base
-#                     dimension_1_base += offset  # Increment the offset for dimension 1 to avoid overlap
-#                     color_txt = 'b-'
-#                 else:
-#                     # For higher dimensions, no need to stagger, just use the dimension as the y-position
-#                     color_txt = 'r-'
-#
-#                     y_position = dimension_2_base
-#                     dimension_2_base += offset  # Increment the offset for dimension 1 to avoid overlap
-#
-#
-#                 # Plot a horizontal line for each feature
-#                 plt.plot([birth, death], [y_position, y_position], color_txt, lw=2)
-#
-#                 # Store the y-position for tick labeling
-#                 y_positions.append(y_position)
-#
-#             # Fix the y-ticks to ensure correct labeling
-#             yticks = [0, dimension_1_base]  # Standard positions for dimensions 0 and 1
-#             yticklabels = ['Dimension 0', 'Dimension 1']
-#
-#             plt.yticks(yticks, yticklabels)
-#
-#             # Add labels and title
-#             plt.xlabel('Filtration Value')
-#             plt.ylabel('Homology Dimension')
-#             plt.title(f'Staggered Barcode of Filtered Persistence Diagram, interval: {j}, animal: {folder_str.split("/")[-4]}')
-#
-#             # Add grid lines for better readability
-#             plt.grid(True, linestyle='--', alpha=0.7)
-#             plt.savefig(folder_str + '/barcode_fold_filtered_h2_interval' + str(j) + '.png', dpi=300, bbox_inches='tight')
-#             # Show the plot
-#             # plt.show()
-#             plt.close('all')
-#
-#
-#             plot_barcode(dgm_gtda[0], 1,fold = i,  save_dir=folder_str)
-#             plot_barcode(dgm_gtda[0], 2,fold=i, save_dir=folder_str)
-#             plot_barcode(dgm_gtda[0], 0, fold= i, save_dir=folder_str)
-#             dgm_gtda_df_filtered.to_csv(folder_str + '/dgm_df_filtered_fold_h2' + '_interval_' + str(j) + '.csv')
-#             np.save(folder_str + '/dgm_fold_h2' + '_interval_' + str(j) + '.npy', dgm)
-#
-#           # plot_barcode(diagrams, 1)
-#         # plt.show()
-#         # plt.close('all')
-#     #save pairs_list
-#     # np.save(folder_str + '/pairs_list_h2.npy', pairs_list)
-#     if use_ripser:
-#         with open(folder_str + '/pairs_list_h2.pkl', 'wb') as f:
-#             pickle.dump(pairs_list, f)
-#
-#     #save the dgm_dict
-#     else:
-#         with open(folder_str + '/dgm_dict_h2.pkl', 'wb') as f:
-#             pickle.dump(dgm_dict, f)
-#
-#     return pairs_list
 
 
 def main():
--- barcode_analysis/helpers/utils.py
+++ barcode_analysis/helpers/utils.py
@@ -1,5 +1,7 @@
 import numpy as np
-
+import numpy as np
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
 def create_folds(n_timesteps, num_folds=5, num_windows=10):
     n_windows_total = num_folds * num_windows
     window_size = n_timesteps / n_windows_total
@@ -25,3 +27,63 @@ def create_folds(n_timesteps, num_folds=5, num_windows=10):
         print(f'Ratio of train to test indices is {ratio}')
 
     return folds
+
+def fit_sinusoid_data_per_interval(df, save_dir, start_indices, end_indices):
+    """
+    Fit a sinusoidal function to the data in each interval and plot the results.
+
+    Parameters
+    ----------
+    df: pd.DataFrame
+        DataFrame containing the data to fit.
+    save_dir: str
+        Directory to save the plot.
+    start_indices: list
+        List of start indices for each segment.
+    end_indices: list
+        List of end indices for each segment.
+    """
+    def sinusoidal(x, A, B, C, D):
+        return A * np.sin(B * x + C) + D
+
+    def calculate_goodness_of_fit(x_data, y_data, y_fitted):
+        residuals = y_data - y_fitted
+        ssr = np.sum(residuals ** 2)
+        sst = np.sum((y_data - np.mean(y_data)) ** 2)
+        r_squared = 1 - (ssr / sst)
+        return r_squared
+
+    for idx, (start, end) in enumerate(zip(start_indices, end_indices)):
+        segment_df = df[(df['Interval'] >= start) & (df['Interval'] <= end)]
+        heatmap_data = segment_df.pivot_table(index='Interval', columns='Dimension', values='death_minus_birth', aggfunc='mean')
+
+        fit_params = {}
+        for dim in heatmap_data.columns:
+            x_data = heatmap_data.index.values
+            y_data = heatmap_data[dim].values
+            initial_guess = [1, 1, 0, np.mean(y_data)]
+            try:
+                params, _ = curve_fit(sinusoidal, x_data, y_data, p0=initial_guess)
+            except Exception as e:
+                print(f'Failed to fit sinusoidal function for dimension {dim} in interval {start}-{end}, error: {e}')
+                continue
+
+            fit_params[dim] = params
+            r_squared = calculate_goodness_of_fit(x_data, y_data, sinusoidal(x_data, *params))
+            print(f'R-squared for dimension {dim} in interval {start}-{end}: {r_squared}')
+
+            plt.figure(figsize=(10, 6))
+            plt.plot(x_data, y_data, 'bo', label='Original Data')
+            plt.plot(x_data, sinusoidal(x_data, *params), 'r-', label='Fitted Function')
+            plt.title(f'Sinusoidal Fit for Homology Dimension {dim} in Interval {start}-{end}')
+            plt.xlabel('Interval (j)')
+            plt.ylabel('Mean Death - Birth')
+            plt.legend()
+            plt.savefig(f'{save_dir}/sinusoidal_fit_dimension_{dim}_interval_{start}_{end}.png', dpi=300, bbox_inches='tight')
+            plt.show()
+            plt.close('all')
+
+        for dim, params in fit_params.items():
+            print(f'Dimension {dim} in interval {start}-{end}: A={params[0]}, B={params[1]}, C={params[2]}, D={params[3]}')
+
+    return fit_params
